e34c7445cfeda9bfb485c9f3f6e1d4b35ba588d0
sedatDemiriz <sedatdemiriz97@gmail.com>
2023-12-13 19:57:26 -0800

    feat: move coding layout files and make appropriate changes to the launch script

--- .config/scripts/coding-layout.sh
+++ .config/scripts/coding-layout.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-i3-msg "workspace 1; append_layout ~/.i3/layouts/monitor1.json; exec alacritty; exec alacritty"
-i3-msg "workspace 2; append_layout ~/.i3/layouts/monitor2.json; exec firefox"
+i3-msg "workspace 1; append_layout ~/.config/i3/layouts/coding-monitor1.json; exec alacritty; exec alacritty"
+i3-msg "workspace 2; append_layout ~/.config/i3/layouts/coding-monitor2.json; exec firefox"
